672c8e45a8c201eb983970e485803fc4927e8eae
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2023-09-24 13:33:11 +0530

    __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -412,7 +412,7 @@ def Fallen_about_callback(update: Update, context: CallbackContext):
                         ),
                         InlineKeyboardButton(
                             text="ɢɪᴛʜᴜʙ",
-                            url="https://t.me/AGORAWORLD",
+                            url="https://t.me/HeartBeat_Offi",
                         ),
                     ],
                     [
