ad14c6dc265b02e50ba70b3bf0b191a07cfa979d
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2023-09-24 12:44:33 +0530

    __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -45,7 +45,7 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph/file/8c78951f44aa036252af9.jpg"
+        "START_IMG", "https://images.hdqwalls.com/wallpapers/agent-cat-girl-4k-jw.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "kimjikoinsupport")
