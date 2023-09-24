5b3b77725284b453bf60752fe7d4b0fd04fc2160
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2023-09-24 12:53:15 +0530

    config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,27 +2,27 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_ID = 8045459
+    API_HASH = "e6d1f09120e17a4372fe022dde88511b"
 
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = "8PSQY07AG6Y5P6SN"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://arfzmkcz:42i-_S_KvPgvm_soGuX-KbbEbqG5QO-T@raja.db.elephantsql.com/arfzmkcz"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1001515341564)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://zewdatabase:ijoXgdmQ0NCyg9DO@zewgame.urb3i.mongodb.net/ontap?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://graph.org/file/6ed3863209d0be9c968de.jpg"
+    START_IMG = "https://images.hdqwalls.com/wallpapers/agent-cat-girl-4k-jw.jpg"
 
-    SUPPORT_CHAT = "agoraworld"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "HeartBeat_Offi"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 6181817811  # User id of your telegram account (Must be integer)
+    OWNER_ID = 1281282633  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
